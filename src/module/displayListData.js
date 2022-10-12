968916228313cfde9d99183a70272f35cc273612
MudasirSherwani <mudasir9757@gmail.com>
2022-10-12 13:20:16 +0500

    Add index.js and dom js file to show score list

---
+++ src/module/displayListData.js
@@ -0,0 +1,14 @@
+
+const ScoreList = document.getElementById('score-list-id');
+// Score List Element Creation here
+const ShowScoreList = (elements) => {
+  const div = `<div><div class="list-section">
+                    <div class="list-elements">
+                        <h4 class="list-name">${elements.Name} : ${elements.Score}</h4>
+                    </div>
+                </div></div><hr></div>`;
+  const ListElements = ScoreList.innerHTML + div;
+  ScoreList.innerHTML = ListElements;
+};
+
+export default ShowScoreList;
