448b6e505ca3a6aa82f1fda2ea7e9ca6466d1618
MudasirSherwani <mudasir9757@gmail.com>
2022-10-14 04:14:05 +0500

    Update displayListData.js file

--- src/module/displayListData.js
+++ src/module/displayListData.js
@@ -1,14 +1,23 @@
+import getData from './getData.js';
 
 const ScoreList = document.getElementById('score-list-id');
 // Score List Element Creation here
 const ShowScoreList = (elements) => {
   const div = `<div><div class="list-section">
                     <div class="list-elements">
-                        <h4 class="list-name">${elements.Name} : ${elements.Score}</h4>
+                        <h4 class="list-name">${elements.user} : ${elements.score}</h4>
                     </div>
                 </div></div><hr></div>`;
   const ListElements = ScoreList.innerHTML + div;
   ScoreList.innerHTML = ListElements;
 };
 
-module.exports =  ShowScoreList;
+const DisplayData = async () => {
+  const data = await getData();
+  ScoreList.textContent = '';
+  data.forEach((item) => {
+    ShowScoreList(item);
+  });
+};
+
+export default DisplayData;
